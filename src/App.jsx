479b56eec4d9dd60365d7002f9254de953c9f70b
vantruong1512 <truongnd0199@gmail.com>
2025-08-05 19:07:34 +0700

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,49 @@
-import React from 'react';
-import CountdownTimer from './CountdownTimer';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setUsers(data))
+      .catch(err => console.error('Lỗi khi fetch users:', err));
+  }, [users]); 
+
   return (
-    <div className="App">
-      <h1>Đồng hồ đếm ngược</h1>
-      <CountdownTimer />
+    <div style={{ padding: '20px' }}>
+      <table border="1" cellPadding="10" cellSpacing="0">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Họ tên</th>
+            <th>Username</th>
+            <th>Email</th>
+            <th>Địa chỉ</th>
+            <th>Điện thoại</th>
+            <th>Website</th>
+            <th>Công ty</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map(user => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.name}</td>
+              <td>{user.username}</td>
+              <td>{user.email}</td>
+              <td>
+                {user.address.street}, {user.address.suite},<br />
+                {user.address.city}, {user.address.zipcode}
+              </td>
+              <td>{user.phone}</td>
+              <td>{user.website}</td>
+              <td>{user.company.name}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
